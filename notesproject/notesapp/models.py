d8e12eb200155f59276ff0e3b882888516c5ff87
anisha <anishakomal2001@gmail.com>
2025-11-15 23:54:18 +0530

    adding gitignore

--- notesproject/notesapp/models.py
+++ notesproject/notesapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+from datetime import datetime
+
+class Note(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    created_on = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
--- notesproject/notesapp/urls.py
+++ notesproject/notesapp/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import note
 
 urlpatterns = [
-    path('',note),
+    path('', note, name='note'),
 ]
